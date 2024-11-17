23354813ad8246fe33cb2ff3e319f7930c5ab918
thehiddenrocky <akshenndragarg17@gmail.com>
2024-11-17 23:10:44 +0200

    Add AI Marketing Agent project with implementation& data

---
+++ AI-marketing-agent/config.py
@@ -0,0 +1,9 @@
+# config.py
+
+# Reddit API Configuration
+REDDIT_CONFIG = {
+    'client_id': 'e9ak0otF8Om8_nGBpNfHZA',
+    'client_secret': 'u1gxgyKgUBwaJkONZEiFfZyQyCCx-A',
+    'user_agent': 'analyse-market/1.0 (by /u/Affectionate_Buddy43)',
+    'redirect_uri': 'http://localhost:8080'
+}
